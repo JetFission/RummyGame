20cb7a9543064b0541302a6f96cb2731094e34f1
asilfies <asilfies@users.noreply.github.com>
2017-04-10 08:44:43 -0400

    Update Rummy.cpp
    added a print function for testing

--- Rummy.cpp
+++ Rummy.cpp
@@ -1,37 +1,11 @@
 #include <iostream>
-
 using namespace std;
-
-class Card {
-private:
-  string suit;
-  string value;
-public:
-  Card(string, string);
-};
-
-Card::Card(string s, string v) {
-  if ((s == "Hearts") | (s == "Clubs") | (s == "Diamonds") | (s == "Spades")) {
-    suit = s;
-  } else {
-    cout << "Invalid suit";
-  }
-
-  if ((v == "Ace") | (v == "2") | (v == "3") | (v == "4") | (v == "5") | (v == "6") |
-     (v == "7") | (v == "8") | (v == "9") | (v == "10") | (v == "Jack") | (v == "Queen") |
-     (v == "King")) {
-
-    value = v;
-  } else {
-    cout << "Invalid value";
-  }
+class Card {private:  string suit;  string value;public:  Card(string, string);  void Print();};
+Card::Card(string s, string v) {  if ((s == "Hearts") | (s == "Clubs") | (s == "Diamonds") | (s == "Spades")) {    suit = s;  } else {    cout << "Invalid suit";  }
+  if ((v == "Ace") | (v == "2") | (v == "3") | (v == "4") | (v == "5") | (v == "6") |     (v == "7") | (v == "8") | (v == "9") | (v == "10") | (v == "Jack") | (v == "Queen") |     (v == "King")) {
+    value = v;  } else {    cout << "Invalid value";  }}
+void Card::Print(){cout << value << " of " << suit << endl;
 }
-
 int main() {
-
-  string suit = "Hearts";
-  string value = "King";
-  Card test(suit, value);
-
-  return 0;
-}
+  string suit = "Hearts";  string value = "King";  Card test(suit, value);  test.Print();
+}}
