e00177dcbe5598107f2681521d2e12e9a3d86ba5
Guillermo Briceno <guillermofbriceno@gmail.com>
2017-04-12 08:32:40 -0400

    Update Deck.cpp

--- Deck.cpp
+++ Deck.cpp
@@ -1,1 +1,34 @@
+#include <iostream>       // std::cout
+#include <vector>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <random>
+#include <ctime>        // std::time
+#include <cstdlib>
 
+using namepsace std;
+
+int myrandom(int i) { return rand() % i; }
+
+class Deck{
+private:
+  vector<Card> deck;
+public:
+  void shuffle();
+  void addCard(Card card);
+  Card removeTopCard();
+  void populateDeck();
+};
+
+void Deck::shuffle() {
+  random_shuffle(deck.begin(), deck.end() , myrandom);
+}
+
+void Deck::addCard(Card card) {
+  deck.push_back(card);
+}
+
+void removeTopCard() {
+  return deck.pop_back();
+}
