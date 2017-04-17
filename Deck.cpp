0e2e1c90b3f99ea420d1dabf8b18956343856b5d
rchurch3 <rchurch3@uncc.edu>
2017-04-16 22:09:21 -0400

    Update Deck.cpp

--- Deck.cpp
+++ Deck.cpp
@@ -19,7 +19,13 @@ public:
   void addCard(Card card);
   void removeTopCard();
   void populateDeck();
+  void dealHands(); //initializes both the computers and players decks with 10 cards each
 };
+
+void Deck::dealHands()
+{
+  
+}
 
 void Deck::shuffle() {
   random_shuffle(deck.begin(), deck.end() , myrandom);
@@ -29,11 +35,26 @@ void Deck::addCard(Card card) {
   deck.push_back(card);
 }
 
-void removeTopCard() {
+void Deck::emoveTopCard() {
   deck.erase(deck.begin(), deck.begin()+1); //removes card from deck. For instance, an object mainDeck of type Deck in main function
 }
 
-void populateDeck()
+void Deck::populateDeck()
 {
-  
+  Card d; //used to load cards into the vector
+
+	char suit[4] = { 'h', 'd', 'c', 's' };
+	char card[13] = { '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' };
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 13; j++)
+		{
+			d.suit = suit[i];
+			d.card = card[j];
+
+			deck.push_back(d);
+		}
+	}
+
 }
