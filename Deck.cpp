7346d18682bac766179179ca093779f452fb7343
rchurch3 <rchurch3@uncc.edu>
2017-04-16 22:21:57 -0400

    Update Deck.cpp

--- Deck.cpp
+++ Deck.cpp
@@ -14,21 +14,36 @@ int myrandom(int i) { return rand() % i; }
 class Deck{
 private:
   vector<Card> deck;
+  static vector<Card> theDeck; //each player gets a copy of the revised deck
 public:
   void shuffle();
   void addCard(Card card);
   void removeTopCard();
   void populateDeck();
   void dealHands(); //initializes both the computers and players decks with 10 cards each
+  void printStuff() //used to verify stuff
 };
+
+void Deck::printStuff()
+{
+	
 
 void Deck::dealHands()
 {
-  
+  for(int i = 0; i < 10; i++) //load players hand and delete the cards taken from the deck
+    {
+        inter.card = deck[i].card;
+        inter.suit = deck[i].suit;
+
+        computer.push_back(inter);
+
+        theDeck.erase(theDeck.begin(), theDeck.begin()+1);
+
+}
 }
 
 void Deck::shuffle() {
-  random_shuffle(deck.begin(), deck.end() , myrandom);
+  random_shuffle(theDeck.begin(), theDeck.end() , myrandom);
 }
 
 void Deck::addCard(Card card) {
@@ -53,7 +68,7 @@ void Deck::populateDeck()
 			d.suit = suit[i];
 			d.card = card[j];
 
-			deck.push_back(d);
+			theDeck.push_back(d);
 		}
 	}
 
