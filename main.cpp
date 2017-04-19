cad18df52f1aa2071a35a6334d44b00efcc00b9d
epswepston <epswepston@gmail.com>
2017-04-19 09:13:22 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,25 +7,24 @@ using namespace std;
 int Rules();
 
 int main() {
-	string answer = "null";
+	string answer; //variable to check options with
 	cout << "Welcome to Gin Rummy, please select an option:\n\n";
 	cout << "Play:\n-Start the game\n\n";
 	cout << "Rules:\n-How to play the game\n\n";
 	cout << "Exit:\n-Closes the Game\n\n";
 	cin >> answer;
 	cout << endl;
-	ofstream GinRules;
-	int menuloop = 1;
+	bool menuloop = true;
 	
-	while (menuloop == 1){
-	menuloop = 0;
+	while (menuloop == true){
 	
+	//multiple ways to input 'play'
 	if((answer == "Play") || (answer == "play") || (answer == "P") || (answer == "p")){
 		menuloop = 0;
 		cout << "Let's play\n";
 		//can call function to start game
 	}
-	
+	//multiple ways to input 'rules'
 	else if((answer == "Rules") || (answer == "rules") || (answer == "R") || (answer == "r")){
 		menuloop = 1;
 		Rules(); //seperate function to open rules.exe
@@ -37,13 +36,13 @@ int main() {
 		cin >> answer;
 		cout << endl;
 	   	}
-	   	
+	//adds exit option
 	else if((answer == "Exit") || (answer == "exit") || (answer == "E") || (answer == "e")){
 		menuloop = 0;
 		cout << "===Program Terminated===" << endl; //seperate function to open rules.exe
 	   	}
 	
-	else{cout << "Please select a valid option\n" << endl;}
+	else{cout << "Please select a valid option\n" << endl; menuloop = false;}
 }
 	
 	return 0;
