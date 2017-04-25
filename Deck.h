d027ae7e1483c9b45f3556642f76221c90bf65a5
Guillermo Briceno <guillermofbriceno@gmail.com>
2017-04-24 22:13:27 -0400

    Update Deck.h

--- Deck.h
+++ Deck.h
@@ -1,1 +1,20 @@
+#ifndef Deck_H
+#define Deck_H
+#include <vector>
 
+class Deck{
+private:
+	vector<Card> deck;
+	Card c;//used as an intermediary
+public:
+	void shuffle();
+	//void addCard(Card card);
+	void removeTopCard();
+	void populateDeck();
+	void dealHands(Deck&,Deck&); //initializes both the computers and players decks with 10 cards each
+	void deleteDeck(); // called only once
+	void printHand(); //used to verify stuff
+	void printDeck();
+};
+
+#endif
