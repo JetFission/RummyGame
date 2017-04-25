c258260bd07d6991e14f3be68155eb583b8ec167
Guillermo Briceno <guillermofbriceno@gmail.com>
2017-04-24 22:51:53 -0400

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -3,19 +3,23 @@
 
 using namespace std;
 
-Card::Card(string s, string v) {
-  if ((s == "Hearts") | (s == "Clubs") | (s == "Diamonds") | (s == "Spades")) {
+Card::Card(char s, char v) {
+  if ((s == 'H') | (s == 'C') | (s == 'D') | (s == 'S')) {
     suit = s;
   } else {
-    cout << "Invalid suit";
+    cout << "Invalid card suit error, check populateDeck()";
   }
 
-  if ((v == "Ace") | (v == "2") | (v == "3") | (v == "4") | (v == "5") | (v == "6") |
-     (v == "7") | (v == "8") | (v == "9") | (v == "10") | (v == "Jack") | (v == "Queen") |
-     (v == "King")) {
+  if ((v == 'A') | (v == '2') | (v == '3') | (v == '4') | (v == '5') | (v == '6') |
+     (v == '7') | (v == '8') | (v == '9') | (v == 'T') | (v == 'J') | (v == 'Q') |
+     (v == 'K')) {
 
     value = v;
   } else {
-    cout << "Invalid value";
+    cout << "Invalid card value error, check populateDeck()";
   }
+}
+
+void Card::showCard() {
+	cout << value << suit;
 }
