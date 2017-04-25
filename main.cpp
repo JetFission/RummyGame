dd0d901c2cc37f0949fca6204eb244023fadc9a7
Guillermo Briceno <guillermofbriceno@gmail.com>
2017-04-24 22:38:18 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -53,7 +53,7 @@ int main() {
 
 void mainLoop() {
   
-	//initalize decks
+	//initalize decks, may have to remove the () on these
 	Deck stockPile();
 	Deck discardPile();
 	Deck playerHand();
@@ -72,11 +72,43 @@ void mainLoop() {
 	//initalize discard pile
 	discardPile.addCard(stockPile.removeTopCard);
 	
+	//persistent variables here
 	boolean finished = false;
+	int playerScore = 0;
+	int AIScore = 0;
 	
 	while (!finished) {
 	
 		//show game UI
+		int csize = 10;	
+		int psize = 10;	
+		char dummychar;
+
+		cout << "****************************************************************" << endl;
+		cout << "* Player Score:" << "000" << "\tStock Size:" << "00" << "\t\tTop Card:" << "XX" << "    *" << endl;
+		cout << "* Computer Score:" << "000" << "\tDiscard Size:" << "00" << "\t\tBottom Card:" << "XX" << " *" <<endl;
+		cout << "****************************************************************" << endl;
+
+		cout << "\t\t\t::Computers Hand::\n\t\t";
+		for (int i = 0; i < csize; ++i) {
+			cout << "XX" << "  ";
+		}
+
+		cout << "\n\n";
+
+		cout << "\t\t\t::Players Hand::\n\t\t";
+		for (int i = 0; i < csize; ++i) {
+			cout << "XX" << "  ";
+		}
+		
+		cout << endl;
+		cout << "****************************************************************" << endl;
+		cout << "\t\t\t   ::Player Actions::" << endl;
+		cout <<"\t\tA)Play\tB)Draw\tC)Move\tD)View\tE)Exit" << endl;
+		cout <<"\t\tPlease Select Action::";
+		cin >> dummychar;
+		cout << endl;
+		
 		//game logic goes here
 		
 	}
