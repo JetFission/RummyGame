5ed1e68740af65a668facfd465473c1a8dc3405f
Guillermo Briceno <guillermofbriceno@gmail.com>
2017-04-27 11:47:55 -0400

    Update Deck.h

--- Deck.h
+++ Deck.h
@@ -7,6 +7,9 @@ class Deck{
 private:
 	vector<Card> deck;
 	Card c;//used as an intermediary
+	
+	//---------------April 27---------------//
+	vector<Card> melds
 public:
 	Deck();
 	void shuffle();
