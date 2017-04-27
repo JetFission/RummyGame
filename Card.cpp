9df442ece192985aae4e253a14b7fb9fc938ea21
rchurch3 <rchurch3@uncc.edu>
2017-04-27 11:15:53 -0400

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -4,30 +4,40 @@
 using namespace std;
 
 Card::Card(char s, char v) {
-  if ((s == 'H') | (s == 'C') | (s == 'D') | (s == 'S')) {
-    suit = s;
-  } else {
-    cout << "Invalid card suit error, check populateDeck()";
-  }
+	if ((s == 'H') | (s == 'C') | (s == 'D') | (s == 'S')) {
+		suit = s;
+	}
+	else {
+		cout << "Invalid card suit error, check populateDeck()";
+	}
 
-  if ((v == 'A') | (v == '2') | (v == '3') | (v == '4') | (v == '5') | (v == '6') |
-     (v == '7') | (v == '8') | (v == '9') | (v == 'T') | (v == 'J') | (v == 'Q') |
-     (v == 'K')) {
+	if ((v == 'A') | (v == '2') | (v == '3') | (v == '4') | (v == '5') | (v == '6') |
+		(v == '7') | (v ==  '8') | (v == '9') | (v == 'T') | (v == 'J') | (v == 'Q') |
+		(v == 'K')) {
 
-    value = v;
-  } else {
-    cout << "Invalid card value error, check populateDeck()";
-  }
+		value = v;
+	}
+	else {
+		cout << "Invalid card value error, check populateDeck()";
+	}
 }
 
 void Card::showCard() {
-	cout << value << suit;
+	cout << value << "\t" << suit;
 }
 
-void Card::setCardPoint(int cp) {
-	cardPoint = cp;
+char Card::getValue()
+{
+	return value;
 }
 
-int Card::getCardPoint() {
-	return cardPoint();
+char Card::getSuit()
+{
+	return suit;
+}
+
+void Card::addCard(char s, char v)
+{
+	suit = s;
+	value = v;
 }
