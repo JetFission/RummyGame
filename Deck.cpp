5541c94f2a9345927a24588772a9e8ad9724816a
rchurch3 <rchurch3@uncc.edu>
2017-04-27 11:16:56 -0400

    Update Deck.cpp

--- Deck.cpp
+++ Deck.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-//---------------4/17/2017--------------//
+//---------------4/26/2017--------------//
 
 int myrandom(int i) { return rand() % i; }
 
@@ -19,12 +19,9 @@ void Deck::dealHands(Deck& other1, Deck& other2)
 {
 	for (unsigned int i = 0; i < 10; i++)
 	{
-		other1.c.card = other2.deck[i].card;
-		other1.c.suit = other2.deck[i].suit;
+		other1.c.addCard(other2.deck[i].getSuit(), other2.deck[i].getValue());
 
 		other1.deck.push_back(other1.c);
-
-		cout << "each card " << other2.deck[i].card << "\t" << other2.deck[i].suit << endl;//test
 
 		other2.deck.erase(other2.deck.begin(), other2.deck.begin() + 1);
 	}
@@ -37,36 +34,38 @@ void Deck::printHand()
 	int count = 0;
 	for (unsigned int i = 0; i < deck.size(); i++)
 	{
-		cout << deck[i].suit << "\t" << deck[i].card << endl;
+		cout << deck[i].getSuit() << "\t" << deck[i].getValue() << endl;
 		cout << endl;
-		count++;
 	}
-	cout << count << endl;
 }
 
 void Deck::printDeck()
 {
+	int count = 0; //test
 	cout << "-------Deck---------" << endl;
-	
+
 
 	for (unsigned int i = 0; i < deck.size(); i++)
 	{
-		cout << deck[i].suit << "\t" << deck[i].card << endl;
+		cout << deck[i].getSuit() << "\t" << deck[i].getValue() << endl;
 		cout << endl;
-		
+		count++;
 	}
+	cout << endl;
+	cout << "The deck now has (testing) " << count << endl;
+	cout << endl;
 }
 
 void Deck::deleteDeck()
 {
-	for (int i = 0; i < 10; i++) 
+	for (int i = 0; i < 10; i++)
 	{
 		deck.erase(deck.begin(), deck.begin() + 1);
 	}
 }
 
 
-void Deck::shuffle() 
+void Deck::shuffle()
 {
 	srand(time(0));
 
@@ -84,27 +83,20 @@ Card Deck::removeTopCard() {
 	return card;
 }
 
-Card Deck::removeCardAt(int num) {
-	Card card = deck[num];
-	vec.erase(vec.begin() + num);
-	return card;
-}
-
 void Deck::populateDeck()
 {
-	Card d; //used to load cards into the vector
 
 	char suit[4] = { 'H', 'D', 'C', 'S' };
-	char card[13] = { '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' };
+	char value[13] = { '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' };
 
 	for (int i = 0; i < 4; i++)
 	{
 		for (int j = 0; j < 13; j++)
 		{
-			d.suit = suit[i];
-			d.card = card[j];
+			c.addCard(suit[i], value[i]);
 
-			deck.push_back(d);
+
+			deck.push_back(c);
 		}
 	}
 }
