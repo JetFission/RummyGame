8146f27236a1cf1fd584fffb5e5996379a022eb7
rchurch3 <rchurch3@uncc.edu>
2017-04-30 21:45:16 -0400

    Update Deck.h

--- Deck.h
+++ Deck.h
@@ -1,27 +1,26 @@
-#ifndef Deck_H
-#define Deck_H
-#include <vector>
-#include "Card.h"
-
-class Deck{
 private:
 	vector<Card> deck;
 	Card c;//used as an intermediary
-	
-	//---------------April 27---------------//
-	vector<Card> melds
+	vector<Card> set; //stores sets belonging to both person and computer
+	vector<Card> temp; //used to temporarily hold Card objects
+	int deadPoints; //used to keep track of the deadwood points for both player and computer
+
 public:
-	Deck();
+	void printSet(); //testing 
+	void printCard(); //used to print a single card
+	int getSizeOfStock(); //returns the size of the stock 
 	void shuffle();
-	void addCard(Card card);
-	Card removeTopCard();
+	void addCard(Deck& card);
+	void addCardFromSpot(Deck& card, int place); //used to add a card from anywhere in the vector
+	void deleteCardFromSpot(int place); //gets rid of card at a certain position 
 	void populateDeck();
 	void dealHands(Deck&, Deck&); //initializes both the computers and players decks with 10 cards each
-	void deleteDeck(); // called only once
+	void deleteDeck(); // called only once. used to delete 10 cards from the deck
 	void printHand(); //used to verify stuff
 	void printDeck();
-	Card removeCardAt(int num);
-	void scanMelds();
+	void scanSet(); //checks hand for sets
+	void deleteCard(); //used to only delete one card
+	int calculateDeadwood(); //used to get the deadwood score after each turn
 };
 
 #endif
